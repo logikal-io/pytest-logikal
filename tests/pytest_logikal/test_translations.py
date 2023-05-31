f4aecb2819963a02d9e9b021f9676b24b39d37ec
GergelyKalmar <gergely.kalmar@logikal.io>
2023-05-31 14:01:28 +0200

    Fix translation checks

--- tests/pytest_logikal/test_translations.py
+++ tests/pytest_logikal/test_translations.py
@@ -1,7 +1,7 @@
 from shutil import copy
 from typing import Callable
 
-from pytest import Pytester, raises
+from pytest import Pytester, mark, raises
 
 from pytest_logikal.plugin import Item, ItemRunError
 from pytest_logikal.translations import TranslationItem, TranslationPlugin
@@ -24,9 +24,17 @@ def test_incomplete(plugin_item: Callable[..., Item]) -> None:
     error.match('41: error: Missing translation')
 
 
-def test_outdated(plugin_item: Callable[..., Item], pytester: Pytester) -> None:
+def test_complete(plugin_item: Callable[..., Item], pytester: Pytester) -> None:
+    contents = {'complete.po': (FILES_DIR / 'complete.po').read_text()}
+    copy(FILES_DIR / 'complete.mo', pytester.path / 'complete.mo')
+    item = plugin_item(plugin=TranslationPlugin, item=TranslationItem, file_contents=contents)
+    item.runtest()  # does not raise an error
+
+
+@mark.parametrize('changed', ['header', 'message'])
+def test_outdated(changed: str, plugin_item: Callable[..., Item], pytester: Pytester) -> None:
     contents = {'outdated.po': (FILES_DIR / 'outdated.po').read_text()}
-    copy(FILES_DIR / 'outdated.mo', pytester.path / 'outdated.mo')
+    copy(FILES_DIR / f'outdated_{changed}.mo', pytester.path / 'outdated.mo')
     item = plugin_item(plugin=TranslationPlugin, item=TranslationItem, file_contents=contents)
     with raises(ItemRunError) as error:
         item.runtest()
