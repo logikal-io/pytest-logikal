aa085bd5cd055183aa5c8572e032dfe1c144034c
GergelyKalmar <gergely.kalmar@logikal.io>
2025-03-01 21:20:16 +0100

    Update dependencies

--- pytest_logikal/licenses.py
+++ pytest_logikal/licenses.py
@@ -54,6 +54,7 @@ ALLOWED_PACKAGES = {
     'attrs': r'^UNKNOWN$',  # license is MIT
     'prettytable': r'^UNKNOWN$',  # license is BSD-3-Clause
     'referencing': r'^UNKNOWN$',  # license is MIT
+    'jeepney': r'^UNKNOWN$',  # license is MIT
 }
 # [1] https://github.com/3YOURMIND/django-migration-linter/issues/290
 # [2] https://bugs.launchpad.net/ubuntu/+source/python-pip/+bug/1635463
