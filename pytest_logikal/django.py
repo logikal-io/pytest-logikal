6a2cf98f37f6805f3b3ed8e8937db9f3125c6ebe
GergelyKalmar <gergely.kalmar@logikal.io>
2025-04-03 18:11:48 +0200

    Remove ignore

--- pytest_logikal/django.py
+++ pytest_logikal/django.py
@@ -48,7 +48,7 @@ def factory_seed() -> None:
     """
     Set a default seed for pytest-factoryboy for deterministic testing. Automatically applied.
     """
-    factory_random.reseed_random(DEFAULT_RANDOM_SEED)  # type: ignore[no-untyped-call]
+    factory_random.reseed_random(DEFAULT_RANDOM_SEED)
 
 
 class LiveURL(Protocol):
