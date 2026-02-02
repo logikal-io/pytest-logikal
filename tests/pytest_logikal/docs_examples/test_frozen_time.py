27b0c3bd6538e4e0631aa6a5ebfa88fb6b6925c6
GergelyKalmar <gergely.kalmar@logikal.io>
2026-02-02 11:09:16 +0100

    Update test time

--- tests/pytest_logikal/docs_examples/test_frozen_time.py
+++ tests/pytest_logikal/docs_examples/test_frozen_time.py
@@ -4,6 +4,6 @@ from time_machine import Traveller
 
 
 def test_frozen_time(frozen_time: Traveller) -> None:
-    destination_time = datetime(1985, 10, 26, 1, 21, 42)
+    destination_time = datetime(2015, 10, 21, 16, 29, 42)
     frozen_time.move_to(destination_time)
     assert datetime.now() == destination_time
